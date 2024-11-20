5eac18b836d315707cd0bd6555e99eee34c2a235
Ansleigh <ansleighong@gmail.com>
2024-11-20 15:35:22 +0800

    Login/SignUp page

---
+++ app.js
@@ -0,0 +1,25 @@
+function DisplayDropdown(){
+    const side = document.querySelector('.dropdown')
+    side.style.display = 'flex'
+}
+
+function CloseDropdown(){
+    const side = document.querySelector('.dropdown')
+    side.style.display = 'none'
+}
+
+function Signup(){
+    const login = document.querySelector('.login')
+    login.style.display = 'none'
+
+    const singup = document.querySelector('.sign')
+    singup.style.display = 'flex'
+}
+
+function Login(){
+    const login = document.querySelector('.login')
+    login.style.display = 'flex'
+
+    const singup = document.querySelector('.sign')
+    singup.style.display = 'none'
+}
