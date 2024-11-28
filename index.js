46cc3bedc4ff4fba52b76aff11831d61da8e2594
Ansleigh <ansleighong@gmail.com>
2024-11-28 13:43:41 +0800

    fixed

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 var flowers = {
     'lilac' :{
-        'img':'pictures/products/lilac-flower.jpg',
-        'name':'Twilight Blossom',
-        'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
-        'price': '$29.90'
+        'flower1':{
+            'img':'pictures/products/lilac-flower.jpg',
+            'name':'Twilight Blossom',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$29.90'},
+
+        'flower2':{
+            'img':'pictures/products/lily-sunflower.jpeg',
+            'name':'Soft Lavender Glow',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$69.00'},
+
+        'flower3':{
+            'img':'pictures/products/lilac-flower.jpg',
+            'name':'Lilac Meadow Magic',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$34.99'},
+
+        'flower4':{
+            'img':'pictures/products/daisy.jpeg',
+            'name':'Hearts in Bloom',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$89.99'},
     },
 
     'roses' :{
-        'img':'pictures/products/roses.jpeg',
-        'name':'Twilight Blossom',
-        'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
-        'price': '$29.90'
+        'flower1':{
+            'img':'pictures/products/roses.jpeg',
+            'name':'Petal Dreams',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$90.90'},
+
+        'flower2':{
+            'img':'pictures/products/lilac-flower.jpg',
+            'name':'Sweet Blooming',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$19.90'},
+
+        'flower3':{
+            'img':'pictures/products/sunflower.jpeg',
+            'name':'Golden Bloom',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$69.90'},
+
+        'flower4':{
+            'img':'pictures/products/daisy.jpeg',
+            'name':'Dewy Dawn Roses',
+            'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
+            'price': '$79.90'},
     },
 
-    'sunflower' :{
-        'img':'pictures/products/lily-sunflower.jpeg',
-        'name':'Twilight Blossom',
-        'description': 'Enchant your loved ones by this dazzling arrangment of flowers',
-        'price': '$29.90'
-    },
 }
 
 
@@ -26,16 +58,22 @@ function showproducts(){
     const container = document.querySelector('.cardbox')
     var html = ``;
     Object.entries(flowers).forEach(([type, details]) => {
-        html += `<div class="card">
-                    <img src="${details['img']}" alt="picture">
-                    <div class="text-box">
-                        <h3><b>${type}</b></h3>
-                        <p>${details['description']}</p>
-                        <p class="price">${details['price']}</p>
-                    </div>
-                </div>`
-    })
+        Object.entries(details).forEach(([flower,content]) =>{
+            html += `<div class="card">
+                        <img src="${content['img']}" alt="picture">
+                        <div class="text-box">
+                            <h3><b>${content['name']}</b></h3>
+                            <p>${content['description']}</p>
+                            <h6 class="price">${content['price']}</h6>
+                        </div>
+                    </div>`;
+    });
+})
     container.innerHTML = html
 };
+
+function showcategories(){
+    
+}
 
 showproducts();
